675f516acb09ac88887fec4a3742c38a90eb161f
Åsa B <backaasa@gmail.com>
2020-01-21 15:35:23 +0100

    added a comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//This is the app component
 
 function App() {
   return (
