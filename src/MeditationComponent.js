a94e48496d321ca6f0966f6c4aee2a31e5bc0afc
Emelie <emelie.edman@hyperisland.se>
2020-01-24 16:16:18 +0100

    created MeditationComponent

---
+++ src/MeditationComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MeditationComponent = () => {
+  return (
+    <div className="meditation-component">
+      <img
+        className="meditation-item1"
+        id="mandala"
+        src={require("./mandala.svg")}
+        alt=""
+      />
+      <h2 className="meditation-item2">MEDITATION</h2>
+      <p className="meditation-item3" id="meditation-instruction">
+        BREATH IN 4 <br></br>HOLD<br></br>BREATH OUT 4<br></br>HOLD
+      </p>
+    </div>
+  );
+};
+
+export default MeditationComponent;
