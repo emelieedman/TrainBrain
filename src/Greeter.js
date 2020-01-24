decd16abcbc3f9b3dcfe846c6820ee6528b84f90
Emelie <emelie.edman@hyperisland.se>
2020-01-24 13:43:07 +0100

    Created a Greeter component

---
+++ src/Greeter.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Greeter = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+  return <h1 className="greeter-text">Good {timeOfDay} sunshine!</h1>;
+};
+
+export default Greeter;
