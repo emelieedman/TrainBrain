2ed2a566e9f8f1e31f2084261c1e0134543d9cb0
Emelie <emelie.edman@hyperisland.se>
2020-01-28 14:25:16 +0100

    removed meditation component from master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./App.css";
-import MeditationComponent from "./MeditationComponent";
 
 function App() {
-  return <MeditationComponent />;
+  return;
 }
 
 export default App;
