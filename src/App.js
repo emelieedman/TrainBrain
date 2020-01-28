b32dc614d90b76c36842db6022007ceedebba48e
Emelie Sabel <emeliesabel@Emelies-MBP.dhcp.sthlm.hyperisland.local>
2020-01-28 15:41:33 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import MeditationComponent from "./MeditationComponent";
+
 import TrainInfoComponent from "./components/TrainInfoComponent/TrainInfoComponent";
 
 function App() {
   return (
     <div>
       <TrainInfoComponent />
-      <MeditationComponent />
     </div>
   );
 }
