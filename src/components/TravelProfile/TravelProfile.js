13a2d214ee8f3505d64ca83bdfdb37da2a0803c1
Emelie <emelie.edman@hyperisland.se>
2020-01-29 12:48:31 +0100

    created the login page

---
+++ src/components/TravelProfile/TravelProfile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./TravelProfile.css";
+
+const TravelProfile = () => {
+  return (
+    <form>
+      <h1 className="headline">CREATE YOUR USER PROFILE!</h1>
+      <div className="input-text">
+        Username:<br></br>
+        <input type="text" name="username"></input>
+      </div>
+      <div className="input-text">
+        Password:<br></br>
+        <input type="password" name="psw"></input>
+      </div>
+      <div className="input-text">
+        From:<br></br>
+        <input type="text" name="from" placeholder="Malmö C"></input>
+      </div>
+      <div className="input-text">
+        To:<br></br>
+        <input type="text" name="to" placeholder="Lund C"></input>
+      </div>
+      <div className="submit-button">
+        <input type="submit" value="SUBMIT"></input>
+      </div>
+    </form>
+  );
+};
+
+export default TravelProfile;
