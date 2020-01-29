dbe65dd9451dc8face1552c8fee97fa6fafbc1f4
Emelie <emelie.edman@hyperisland.se>
2020-01-29 12:47:58 +0100

    added travel-profile component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import Greeter from "./components/Greeter/Greeter";
 import Meditation from "./components/Meditation/Meditation";
 import TrainInfoPage from "./components/TrainInfoPage/TrainInfoPage";
+import TravelProfile from "./components/TravelProfile/TravelProfile";
 
 function App() {
   return (
     <div>
-      <Greeter />
+      <TravelProfile />
+      {/* <Greeter />
       <TrainInfoPage />
-      <Meditation />
-
+      <Meditation /> */}
     </div>
   );
 }
