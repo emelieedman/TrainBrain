8cf7a716ee4954c0bcfbf087a8202948fb99ed68
Emelie <emelie.edman@hyperisland.se>
2020-01-30 15:30:24 +0100

    removed sunchine

--- src/components/Greeter/Greeter.js
+++ src/components/Greeter/Greeter.js
@@ -13,11 +13,7 @@ const Greeter = () => {
   } else {
     timeOfDay = "NIGHT";
   }
-  return (
-    <h1 className="greeter-text">
-      GOOD {timeOfDay} <br></br> !
-    </h1>
-  );
+  return <h1 className="greeter-text">GOOD {timeOfDay}!</h1>;
 };
 
 export default Greeter;
