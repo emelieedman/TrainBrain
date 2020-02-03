c8181d315db0780f5a4360847c5f56ccc43a422c
Essi Suomalainen <essi.suomalainen@hyperisland.se>
2020-02-03 16:02:13 +0100

    open-close animation on meditation component

--- src/components/Meditation/Meditation.js
+++ src/components/Meditation/Meditation.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./meditation.css";
 
 const MeditationComponent = () => {
+  const [animation, setAnimation] = useState(false);
+  
+
+function toggleButton() {
+ setAnimation(!animation);
+ 
+
+}
+
   return (
-    <div className="meditation-component">
+      
+     <div className = {!animation ? ( "meditation-component reverse-animation") : " meditation-component is-animated" } onClick={() => toggleButton()}>
+       
       <img
         className="meditation-item1"
         id="mandala"
         src={require("./mandala.svg")}
         alt=""
       />
-      <h2 className="meditation-item2">MEDITATION</h2>
+      <h2 className="meditation-item2" >MEDITATION</h2>
       <div id="breath">
       <h5 className="text">BREATHE</h5>
       </div>
-    </div>
+    </div> 
   );
 };
 
